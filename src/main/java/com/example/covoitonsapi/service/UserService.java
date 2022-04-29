ff6f610e4babb7995825ee8a29589445888dc1e7
Oldelaf <72396968+Oldelaf@users.noreply.github.com>
2022-04-29 15:01:54 +0200

    adduser

--- src/main/java/com/example/covoitonsapi/service/UserService.java
+++ src/main/java/com/example/covoitonsapi/service/UserService.java
@@ -1,10 +1,12 @@
 package com.example.covoitonsapi.service;
 
 import com.example.covoitonsapi.dto.UserDto;
+import com.example.covoitonsapi.entity.UserEntity;
 import com.example.covoitonsapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,5 +18,22 @@ public class UserService implements IUserService {
     @Override
     public List<UserDto> getAllUser() {
         return null;
+    }
+
+    @Override
+    public Boolean add(UserDto dto) {
+        UserEntity entity = new UserEntity();
+        entity.setFirstname(dto.getFirstname());
+        entity.setLastname(dto.getLastname());
+        entity.setEmail(dto.getEmail());
+        entity.setEmployee_code(dto.getEmployee_code());
+        entity.setPassword(dto.getPassword());
+        entity.setUpdated_at(LocalDateTime.now());
+        entity.setCreated_at(LocalDateTime.now());
+        entity.setIs_admin(false);
+
+        repository.saveAndFlush(entity);
+
+        return true;
     }
 }
