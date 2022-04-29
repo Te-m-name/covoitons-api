15f85bf9c37addb81d2200baf0461a30d4ed9828
Oldelaf <72396968+Oldelaf@users.noreply.github.com>
2022-04-29 09:12:09 +0200

    first commit

---
+++ src/main/java/com/example/covoitonsapi/CovoitonsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.covoitonsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovoitonsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovoitonsApiApplication.class, args);
+    }
+
+}
