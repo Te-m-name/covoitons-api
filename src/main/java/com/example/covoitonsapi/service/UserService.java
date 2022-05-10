d87ae81f6e07bc141d8cd6a0f04f695f62c9f289
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-05-10 23:18:43 +0200

    add authentication

--- src/main/java/com/example/covoitonsapi/service/UserService.java
+++ src/main/java/com/example/covoitonsapi/service/UserService.java
@@ -3,17 +3,50 @@ package com.example.covoitonsapi.service;
 import com.example.covoitonsapi.dto.UserDto;
 import com.example.covoitonsapi.entity.UserEntity;
 import com.example.covoitonsapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
-public class UserService implements IUserService {
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserService implements IUserService, UserDetailsService {
 
     @Autowired
     private UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = repository.findByEmail(email);
+        if(user == null) {
+            log.error("User not found in database");
+            throw new UsernameNotFoundException("User not found in database");
+        } else {
+            log.info("User found in database: {}", email);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        if(user.getIs_admin() == true) {
+            authorities.add(new SimpleGrantedAuthority("admin"));
+        } else {
+            authorities.add(new SimpleGrantedAuthority("user"));
+        }
+
+        return new org.springframework.security.core.userdetails.User(email, user.getPassword(), authorities);
+    }
 
     @Override
     public List<UserDto> getAllUser() {
@@ -27,7 +60,7 @@ public class UserService implements IUserService {
         entity.setLastname(dto.getLastname());
         entity.setEmail(dto.getEmail());
         entity.setEmployee_code(dto.getEmployee_code());
-        entity.setPassword(dto.getPassword());
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
         entity.setUpdated_at(LocalDateTime.now());
         entity.setCreated_at(LocalDateTime.now());
         entity.setIs_admin(false);
@@ -36,4 +69,5 @@ public class UserService implements IUserService {
 
         return true;
     }
+
 }
