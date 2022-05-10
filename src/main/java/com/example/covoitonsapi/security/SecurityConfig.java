df359c7f031d011ed28f8269fbf086b56f4ee426
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-05-11 00:01:32 +0200

    add authorization

--- src/main/java/com/example/covoitonsapi/security/SecurityConfig.java
+++ src/main/java/com/example/covoitonsapi/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.covoitonsapi.security;
 
 import com.example.covoitonsapi.filter.CustomAuthenticationFilter;
+import com.example.covoitonsapi.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -37,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // http.authorizeRequests().antMatchers(GET, "/ride/**").hasAnyAuthority("user");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
