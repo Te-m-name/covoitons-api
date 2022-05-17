a166358f6703e567c6c64a322ee8ac225c717696
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2022-05-17 19:00:04 +0200

    add date to ride dto and change for research

--- src/main/java/com/example/covoitonsapi/repository/RideRepository.java
+++ src/main/java/com/example/covoitonsapi/repository/RideRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface RideRepository extends JpaRepository<RideEntity, Integer> {
 
-    @Query(value = "select * from rides where city = ?1", nativeQuery = true)
-    List<RideEntity> getRidesByCity(String city);
+    @Query(value = "select * from rides where city = ?1 and home_to_office = ?2 and date = ?3", nativeQuery = true)
+    List<RideEntity> findByCityAndHome_to_officeAndDeparture_time(String city, Boolean home_to_office, LocalDate date);
 
-    List<RideEntity> findByCity(String city);
-
-    List<RideEntity> findTop4ByOrderByIdDesc();
+    List<RideEntity> findTop5ByOrderByIdDesc();
 }
