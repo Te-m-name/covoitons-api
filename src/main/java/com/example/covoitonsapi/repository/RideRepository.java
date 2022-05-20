2ab78e81ee635c4f80655aec4e1efb2689e04dab
Gymrick <91668757+Gymrick@users.noreply.github.com>
2022-05-20 09:47:50 +0200

    re conflict resolve

--- src/main/java/com/example/covoitonsapi/repository/RideRepository.java
+++ src/main/java/com/example/covoitonsapi/repository/RideRepository.java
@@ -16,10 +16,5 @@ public interface RideRepository extends JpaRepository<RideEntity, Integer> {
     @Query(value = "select * from rides where city = ?1 and home_to_office = ?2 and date = ?3", nativeQuery = true)
     List<RideEntity> findByCityAndHome_to_officeAndDeparture_time(String city, Boolean home_to_office, LocalDate date);
 
-<<<<<<< HEAD
-    List<RideEntity> findByCity(String city);
-
-=======
     List<RideEntity> findTop5ByOrderByIdDesc();
->>>>>>> 9b9c8919a3557df7a77b631341f757797eb6a858
 }
