82208b7b742d15c5f3f94260a5cb0dc320a647e9
Oldelaf <72396968+Oldelaf@users.noreply.github.com>
2022-06-08 09:51:59 +0200

    feature/trajetRecurrent

---
+++ src/main/java/com/example/covoitonsapi/batchJob/Schedule.java
@@ -0,0 +1,24 @@
+package com.example.covoitonsapi.batchJob;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Schedule {
+
+    @Scheduled( initialDelay = 3 * 1000, fixedDelay = 2 * 1000)
+    public void writeCurrentTime() {
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss sss");
+        Date now = new Date();
+
+        String nowString = df.format(now);
+
+        System.out.println("Now is: "+ nowString);
+
+    }
+}
